cd665af61549d9bd4deeb58ff32ea06209017331
Avery Caldwell <averycaldwell7@gmail.com>
2024-09-29 15:54:58 -0700

    added filters

--- src/components/Webcam.jsx
+++ src/components/Webcam.jsx
@@ -1,18 +1,37 @@
-// TODO: set up webcam.
-// TODO: apply a simple CSS filter.
-
 import React from "react";
 import Webcam from "react-webcam";
+import { useState } from "react";
 
 const WebcamComponent = () => {
+  const [filter, setFilter] = useState("");
+
+  const videoStyles = {
+    filter: filter,
+    width: "100%", // Adjust the video size to fit your needs
+    height: "auto",
+  };
   return (
-    <div>
-      <Webcam
+    <>
+      {/* <div> */}
+      {/* <Webcam
         width="100%" // Adjust the width or height as needed
         height="auto"
         screenshotFormat="image/jpeg" // For taking screenshots later
-      />
-    </div>
+      /> */}
+      {/* </div> */}
+      <div>
+        <Webcam style={videoStyles} audio={false} />
+
+        <div>
+          <button onClick={() => setFilter("")}>No Filter</button>
+          <button onClick={() => setFilter("grayscale(100%)")}>
+            Grayscale
+          </button>
+          <button onClick={() => setFilter("sepia(100%)")}>Sepia</button>
+          <button onClick={() => setFilter("blur(5px)")}>Blur</button>
+        </div>
+      </div>
+    </>
   );
 };
 
